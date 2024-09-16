43d18cfa10dc95f92947729559f9f07002fddda1
muhammed rifan <rifanmuhammed173@gmail.com>
2024-09-16 10:54:04 +0530

    some updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const birthDate = new Date('2004-07-26');
 const today = new Date();
-const deathDate = new Date(birthDate.getFullYear() + 70, birthDate.getMonth(), birthDate.getDate());
+const deathDate = new Date(birthDate.getFullYear() + 25, birthDate.getMonth(), birthDate.getDate());
 
 const totalWeeks = Math.floor((deathDate - birthDate) / (1000 * 60 * 60 * 24 * 7)); // Total weeks (70 years)
 const completedWeeks = Math.floor((today - birthDate) / (1000 * 60 * 60 * 24 * 7)); // Completed weeks till today
@@ -31,4 +31,25 @@ for (let i = 0; i <= totalWeeks; i++) {
   }
 
   weekContainer.appendChild(weekDiv);
+}
+
+
+function SocialMediaDirect(num){
+
+const linkedIn = "https://www.linkedin.com/in/muhammed-rifan-k-638074257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
+
+if(num==1){
+window.open(linkedIn,'_blank')
+}
+else if(num===2){
+  window.open("https://github.com/muhammedrifankallayi",'_blank')
+}
+else if(num==3){
+window.open(linkedIn,'_blank')
+}else
+if(num==4){
+  window.open("https://leetcode.com/u/rifanax3/",'_blank')
+}
+ 
+
 }
